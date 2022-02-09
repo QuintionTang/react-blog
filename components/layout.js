fd869dccac10d8e3e7e64e5a9ea3ebdfcb0bdf3f
Quintion <QuintionTang@gmail.com>
2022-02-09 11:15:51 +0800

    feat:使用 React Transition Group 增加动画效果

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,47 @@
 import Header from "./header";
 import Footer from "./footer";
+import { useState } from "react";
+import { Transition } from "react-transition-group";
+
+const duration = 300;
+
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+};
+
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+};
 
 export default function Layout({ children, title }) {
+    const [inProp, setInProp] = useState(false);
+    const delay = () => {
+        setTimeout(() => {
+            !inProp && setInProp(!inProp);
+        }, 500);
+    };
+    delay();
     return (
         <>
             <Header title={title} />
-            <main>{children}</main>
+            <main>
+                <Transition in={inProp} timeout={500}>
+                    {(state) => (
+                        <div
+                            style={{
+                                ...defaultStyle,
+                                ...transitionStyles[state],
+                            }}
+                        >
+                            {children}
+                        </div>
+                    )}
+                </Transition>
+            </main>
             <Footer />
         </>
     );
