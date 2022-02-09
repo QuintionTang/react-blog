53705051bab6baa8bbccdc546570d1962c7b5af8
Quintion <QuintionTang@gmail.com>
2022-02-09 20:46:24 +0800

    feat:使用 React Spring 增加动画效果

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,41 @@
 import Header from "./header";
 import Footer from "./footer";
+import { useState } from "react";
+import { useSpring, config, animated } from "react-spring";
 
 export default function Layout({ children, title }) {
+    const [show, setShow] = useState(true);
+    const properties = {
+        to: { opacity: 1 },
+        from: { opacity: 0 },
+        reset: true,
+        delay: 200,
+        config: config.molasses,
+    };
+
+    const toggleText = () => {
+        setShow(!show);
+        api.start({
+            ...properties,
+            to: { opacity: show ? 1 : 0 },
+            from: { opacity: show ? 0 : 1 },
+        });
+    };
+    const [props, api] = useSpring(() => properties);
+
+    const delay = () => {
+        setTimeout(() => {
+            !show && toggleText();
+        }, 500);
+    };
+    delay();
+
     return (
         <>
             <Header title={title} />
-            <main>{children}</main>
+            <main>
+                <animated.div style={props}>{children}</animated.div>
+            </main>
             <Footer />
         </>
     );
