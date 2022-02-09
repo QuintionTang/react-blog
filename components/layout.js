61e18ca1ce40de4e5159647c3e6a0543ddf11216
Quintion <QuintionTang@gmail.com>
2022-02-09 11:32:04 +0800

    feat:使用 React Reveal 增加动画效果

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,25 @@
 import Header from "./header";
 import Footer from "./footer";
+import { useState } from "react";
+import Fade from "react-reveal/Fade";
 
 export default function Layout({ children, title }) {
+    let [show, setShow] = useState(false);
+
+    const delay = () => {
+        setTimeout(() => {
+            !show && setShow(!show);
+        }, 500);
+    };
+    delay();
     return (
         <>
             <Header title={title} />
-            <main>{children}</main>
+            <main>
+                <Fade top when={show}>
+                    {children}
+                </Fade>
+            </main>
             <Footer />
         </>
     );
